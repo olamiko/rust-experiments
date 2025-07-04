c2dac53f88aba0203740b2e6d721a96c7d675adb
Olamide Omolola <omololaolamidex@gmail.com>
2025-07-04 06:49:33 +0200

    updated code to use writer and reader methods directly in serde-json

--- test-serde-json-serialization/src/main.rs
+++ test-serde-json-serialization/src/main.rs
@@ -1,9 +1,7 @@
-//! A simple crate to test the use of serde-json serialization. 
+//! A simple crate to test the use of serde-json serialization.
 use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::BufReader;
-use std::io::Read;
-use std::io::Write;
 
 /// Struct indicating a single movement of a chess piece indicating its direction and the number of squares moved
 #[derive(Debug, Serialize, Deserialize)]
@@ -32,21 +30,17 @@ impl Move {
 // Heavy use of unwrap()! No error handling due to its experimental nature
 fn main() {
     let a = Move::new(1, Direction::SOUTH);
-    let j = serde_json::to_string(&a);
 
     // write to file
-    let mut file = File::create("serialized.txt").unwrap();
-    file.write_all(j.as_ref().unwrap().to_owned().as_bytes())
-        .unwrap();
+    let file = File::create("serialized.txt").unwrap();
+    let _ = serde_json::to_writer(file, &a);
 
     // read from file
     let file = File::open("serialized.txt").unwrap();
-    let mut buf_reader = BufReader::new(file);
-    let mut contents = String::new();
-    buf_reader.read_to_string(&mut contents).unwrap();
+    let buf_reader = BufReader::new(file);
 
     // deserialize
-    let b: Move = serde_json::from_str(&contents).unwrap();
+    let b: Move = serde_json::from_reader(buf_reader).unwrap();
 
     println!("Movement parameters: {:?}", a);
     println!("Deserialized Movement parameters: {:?}", b);
